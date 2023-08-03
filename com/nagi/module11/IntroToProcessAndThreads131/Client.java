2736534f364d9e442408fdb5ef8abe7530e59292
Arekuti Nagireddy <nagi2038@gmail.com>
2023-08-03 16:21:11 +0530

    created client class in131

---
+++ com/nagi/module11/IntroToProcessAndThreads131/Client.java
@@ -0,0 +1,11 @@
+package com.nagi.module11.IntroToProcessAndThreads131;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println(Student.uniName);
+    }
+}
+
+class Student{
+    public static String uniName = "Scaler";
+}
