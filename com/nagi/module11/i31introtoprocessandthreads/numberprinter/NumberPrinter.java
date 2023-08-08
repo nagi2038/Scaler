eb6266fa34288db16fac43cf3a63e75f1adcb0ae
Arekuti Nagireddy <nagi2038@gmail.com>
2023-08-08 11:46:40 +0530

    thread sleep

--- com/nagi/module11/i31introtoprocessandthreads/numberprinter/NumberPrinter.java
+++ com/nagi/module11/i31introtoprocessandthreads/numberprinter/NumberPrinter.java
@@ -7,7 +7,18 @@ public class NumberPrinter implements Runnable {
     public void run(){
         for(int i = 0; i< 100 ; i++)
         {
-            System.out.println(i+1);
+            if(i == 4)
+            {
+                
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+            System.out.println("I'm from new thread");
         }
         
     }
