128fe922fac65427e7455b408b5511f9e2ea6ed8
Arekuti Nagireddy <nagi2038@gmail.com>
2023-08-08 13:08:10 +0530

    removed todo line

--- com/nagi/module11/i31introtoprocessandthreads/numberprinter/NumberPrinter.java
+++ com/nagi/module11/i31introtoprocessandthreads/numberprinter/NumberPrinter.java
@@ -14,7 +14,6 @@ public class NumberPrinter implements Runnable {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
