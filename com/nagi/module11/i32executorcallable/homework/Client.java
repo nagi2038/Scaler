8db25bde6067ed3a0bbb1a81efe73cd85c6a76a0
Arekuti Nagireddy <nagi2038@gmail.com>
2023-08-09 19:51:02 +0530

    HOMEWORK is Done

---
+++ com/nagi/module11/i32executorcallable/homework/Client.java
@@ -0,0 +1,45 @@
+package com.nagi.module11.i32executorcallable.homework;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Client {
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        RandomNumberPrinter rnp = new RandomNumberPrinter();
+        ArrayList<Future<Integer>> farray = new ArrayList<>();
+
+        for (int i = 0; i<12; i++)
+        {// adding all future objects to array list
+            farray.add(service.submit(rnp));
+            System.out.println("Submitting the request.");
+        }
+
+        try {
+            Thread.sleep(20000);
+            System.out.println("Main thread is sleeping for 10 secs");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (Future<Integer> future : farray) {
+            // calling get method on future object
+            try {   
+                System.out.println("****************** GET CALL STARTS ****************");
+                int val = future.get();  
+                System.out.println("Returned value : " + val);           
+            } catch (Exception e) {
+                System.out.println("Error in Code");
+            } 
+        }
+        service.shutdown();
+}
+}
+/*
+ * In above code I have sumbited 12 request
+ * And i have threadpool size of 3
+ * so, first 3 request are executed, since i have only threadpool size of 3
+ * and wait for get call 
+ */
